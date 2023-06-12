0ffc8a3bc5d52acc6b3f85f0a61ed46eb43b95b4
dquangxyz <dq2209@gmail.com>
2023-06-12 18:09:57 +1000

    connect to mongodb

--- environment.d.ts
+++ environment.d.ts
@@ -3,6 +3,7 @@ declare global {
       interface ProcessEnv {
         GOOGLE_ID : string;
         GOOGLE_SECRET: string;
+        MONGODB_URI: string;
         NODE_ENV: 'development' | 'production';
         PORT?: string;
         PWD: string;
