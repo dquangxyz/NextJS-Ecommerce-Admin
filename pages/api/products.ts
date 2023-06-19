c29fb3e20f5cbad2c14ab0697efd02ee94320860
dquangxyz <dq2209@gmail.com>
2023-06-19 15:27:02 +0800

    display all products in a table

--- pages/api/products.ts
+++ pages/api/products.ts
@@ -7,8 +7,9 @@ export default async function handle(req: NextApiRequest, res: NextApiResponse) 
     const { method } = req;
     await mongooseConnect();
     
-
-    if (method === 'POST'){
+    if (method === 'GET'){
+        res.json(await Product.find())
+    } else if (method === 'POST'){
         const { title, description, price } = req.body;
         const productDoc = await Product.create({title, description, price})
         res.json(productDoc);
