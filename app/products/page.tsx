43f39527f1eff4fc02e002d1faea03ad8a347596
dquangxyz <dq2209@gmail.com>
2023-06-21 23:16:46 +0700

    delete an item

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,13 +1,12 @@
 "use client"
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { redirect } from "next/navigation";
 import Layout from "@/components/Layout";
 import Link from "next/link";
 
 interface ProductItem {
-  _id: {
-    $oid: string;
-  };
+  _id: string;
   title: string;
   description: string;
   price: number;
@@ -16,11 +15,35 @@ interface ProductItem {
 export default function Products() {
   const [productsList, setProductsList] = useState<ProductItem[]>([]);
 
+  const [selectedDeleteItem, setSelectedDeleteItem] = useState<string>('');
+  const [showDeleteModal, setShowDeleteModal] = useState<boolean>(false);
+
+  const [goToProduct, setGoToProduct] = useState<boolean>(false);
+
   useEffect(() => {
     axios.get('/api/products').then(res => {
       setProductsList(res.data);
     });
   }, []);
+
+  // handler functions
+  const handleOpenModal = (id: string) => {
+    setShowDeleteModal(true);
+    setSelectedDeleteItem(id);
+  }
+  const handleConfirmDelete = async (confirmed: boolean) => {
+    if (confirmed) {
+      await axios.delete('/api/products?id='+selectedDeleteItem);
+      console.log("Product deleted successfully");
+      setGoToProduct(true);
+    }
+    setShowDeleteModal(false);
+    setSelectedDeleteItem('');
+  };
+
+  if (goToProduct){
+    return redirect ('/products')
+  }
 
   return (
     <Layout>
@@ -34,7 +57,7 @@ export default function Products() {
         </thead>
         <tbody>
           {productsList.map(item => (
-            <tr key={item._id.$oid}>
+            <tr key={item._id}>
               <td>{item.title}</td>
               <td>
                 <Link className="btn-light-blue" href={`/products/edit/${item._id}`}>
@@ -43,15 +66,31 @@ export default function Products() {
                   </svg>
                   Edit
                 </Link>
-                <Link className="btn-red" href={'/products/delete/'+item._id}>
+
+                <div className="delete-button btn-red" onClick={() => handleOpenModal(item._id)}>
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                   </svg>
                   Delete
-                </Link>
+                </div>
               </td>
             </tr>
           ))}
+
+
+          {showDeleteModal && (
+            <div className="modal">
+              <div className="modal-content">
+                <h2>Confirm Delete</h2>
+                <p>Are you sure you want to delete this product (id: ${selectedDeleteItem} )?</p>
+                <div className="modal-buttons">
+                  <button onClick={() => handleConfirmDelete(true)}>Yes</button>
+                  <button onClick={() => handleConfirmDelete(false)}>No</button>
+                </div>
+              </div>
+            </div>
+          )}
+
         </tbody>
       </table>
     </Layout>
--- pages/api/products.ts
+++ pages/api/products.ts
@@ -24,6 +24,11 @@ export default async function handle(req: NextApiRequest, res: NextApiResponse) 
       const { title,description,price,_id } : IProduct = req.body;
       await Product.updateOne({_id}, { title,description,price });
       res.json(true);
+    } else if (method === 'DELETE') {
+      if (req.query?.id) {
+        await Product.deleteOne({_id:req.query?.id});
+        res.json(true);
+      }
     }
 
   } catch (error) {
