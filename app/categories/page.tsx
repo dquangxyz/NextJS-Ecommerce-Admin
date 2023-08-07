1a6922631f81a0eb5a8f0dbe2ab1132e551df4de
dquangxyz <dq2209@gmail.com>
2023-08-07 19:12:59 +1000

    add new category successfully to db

--- app/categories/page.tsx
+++ app/categories/page.tsx
@@ -5,15 +5,35 @@ import Link from "next/link";
 import {useEffect, useState} from "react";
 import axios from "axios";
 
-function Categories() {
+interface CategoryItem {
+    _id: string;
+    name: string;
+    parentCategory: string
+}
+
+export default function Categories() {
+    // local state
     const [name, setName] = useState<string>("");
+    // const [parentCategory,setParentCategory] = useState<string>("");
+    const [categoriesList, setCategoriesList] = useState<CategoryItem[]>([]);
+
+    useEffect(() => {
+        axios.get('/api/categories').then(res => {
+            setCategoriesList(res.data);
+        });
+    }, []);
+
+    // define helper functions
     const saveCategory = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         await axios.post('/api/categories', {
-            name: name
+            name: name,
         });
         setName("");
     };
+
+    const editCategory = (e: CategoryItem) => {console.log("edit button clicked")};
+    const deleteCategory = (e: CategoryItem) => {console.log("delete button clicked")}; 
 
   return (
     <Layout>
@@ -27,13 +47,39 @@ function Categories() {
                     onChange={(e) => setName(e.target.value)}
                     value={name}
                 />
+                {/* <select onChange={ev => setParentCategory(ev.target.value)} value={parentCategory}>
+                    <option value="">No parent category</option>
+                    {categoriesList.map(category => (
+                        <option key={category._id} value={category._id}>{category.name}</option>
+                    ))}
+                </select> */}
             </div>
             <div className="flex gap-1">     
                 <button type="submit" className="bg-blue-900 text-white px-4 py-1 rounded-md">Save</button>
             </div>
         </form>
+
+        <table className="basic mt-4">
+            <thead>
+            <tr>
+                <td>Category name</td>
+                <td>Parent category</td>
+                <td></td>
+            </tr>
+            </thead>
+            <tbody>
+            {categoriesList.map(category => (
+                <tr key={category._id}>
+                    <td>{category.name}</td>
+                    <td>{category.name}</td>
+                    <td>
+                        <button onClick={() => editCategory(category)} className="btn-light-blue">Edit</button>
+                        <button onClick={() => deleteCategory(category)} className="btn-red">Delete</button>
+                    </td>
+                </tr>
+            ))}
+            </tbody>
+        </table>
     </Layout>
   )
 }
-
-export default Categories
--- pages/api/categories.ts
+++ pages/api/categories.ts
@@ -2,17 +2,19 @@ import { Category } from '@/models/Category';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 interface ICategory {
-    name: string
+    name: string,
 }
   
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
     const { method } = req;
     
     if (method === 'GET'){
-        res.json("get")
+        res.json(await Category.find());
     } else if (method === 'POST'){
         const { name } : ICategory = req.body;
-        const categoryDoc = await Category.create({name: name});
+        const categoryDoc = await Category.create({
+            name: name,
+        });
         res.json(categoryDoc);
     } else if (method === 'PUT') {
         res.json("put")
