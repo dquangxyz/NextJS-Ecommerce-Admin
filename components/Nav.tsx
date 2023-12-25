4606418d06141a9b82823e0c9956f7ea6115afe8
dquangxyz <dq2209@gmail.com>
2023-12-25 17:29:15 +1100

    fix bugs about nextauth when sign out. clear the session when signin out

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -11,8 +11,8 @@ export default function Nav() {
   const router = useRouter();
   const pathname = usePathname();
   async function logout() {
-    await router.push('/');
-    await signOut();
+    // await router.push('/');
+    await signOut({ redirect: false, callbackUrl: '/' });
   }
   return (
     // <aside className={(show?'left-0':'-left-full')+" top-0 text-gray-500 p-4 fixed w-full bg-bgGray h-full md:static md:w-auto transition-all"}>
@@ -52,7 +52,7 @@ export default function Nav() {
           </svg>
           Settings
         </Link>
-        <button onClick={logout} className={inactiveLink}>
+        <button onClick={() => logout()} className={inactiveLink}>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
           <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
           </svg>
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,13 +3,32 @@ import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
+const adminEmails = ['dq2209@gmail.com']
+
 export default NextAuth({
-    providers: [
-      GoogleProvider({
-        clientId: process.env.GOOGLE_ID,
-        clientSecret: process.env.GOOGLE_SECRET
-      })
-    ],
-    adapter: MongoDBAdapter(clientPromise)
-  })
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET
+    })
+  ],
+  adapter: MongoDBAdapter(clientPromise),
+  callbacks: {
+    async session({ session, token, user }: any) {
+      console.log({ session, token, user });
+      if (session?.user?.email){
+        if (adminEmails.includes(session.user.email)) {
+          console.log("approved");
+          return session;
+        } else {
+          console.log("not approved - not found the correct email")
+          return null;
+        } 
+      } else {
+        console.log("not approved - no session")
+        return null;
+      }     
+    }
+  }
+})
 
